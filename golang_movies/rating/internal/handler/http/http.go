61a31afb8f9c5fe38ca10fff0c1362636cc39899
Jose Martinez <44812411+JoseM111@users.noreply.github.com>
2023-01-14 08:24:41 -0500

    EPIC0003: status -> refactoring (Jan 14th)

--- golang_movies/rating/internal/handler/http/http.go
+++ golang_movies/rating/internal/handler/http/http.go
@@ -5,6 +5,7 @@ import (
 	"errors"
 	"log"
 	"net/http"
+	"strconv"
 	
 	"golangmovies.com/rating/internal/controller/rating"
 	"golangmovies.com/rating/internal/repository"
@@ -47,15 +48,40 @@ func (handler *Handler) Handle(responseWriter http.ResponseWriter, request *http
 	
 	switch request.Method {
 	case http.MethodGet:
-		v, err := handler.ctrl.GetAggregatedRating(request.Context(), recordID, recordType)
-		if err != nil && errors.Is(err, repository.ErrNotFound) {
-			responseWriter.WriteHeader(http.StatusNotFound)
-			return
+		{
+			v, err := handler.ctrl.GetAggregatedRating(request.Context(), recordID, recordType)
+			if err != nil && errors.Is(err, repository.ErrNotFound) {
+				responseWriter.WriteHeader(http.StatusNotFound)
+				return
+			}
+			
+			if err := json.NewEncoder(responseWriter).Encode(v); err != nil {
+				log.Printf("Response encode error: %#v\n", err)
+			}
 		}
-		
-		if err := json.NewEncoder(responseWriter).Encode(v); err != nil {
-			log.Printf("Response encode error: %#v\n", err)
+	case http.MethodPut:
+		{
+			userID := model.UserID(request.FormValue("userId"))
+			value, err := strconv.ParseFloat(request.FormValue("value"), 64)
+			
+			if err != nil {
+				// STATUS CODE: 400
+				responseWriter.WriteHeader(http.StatusBadRequest)
+				return
+			}
+			
+			if err := handler.ctrl.PutRating(request.Context(), recordID, recordType, &model.Rating{
+				UserID: userID,
+				Value:  model.RatingValue(value),
+			}); err != nil {
+				log.Printf("Repository put error: %#v\n", err)
+				// STATUS CODE: 500
+				responseWriter.WriteHeader(http.StatusInternalServerError)
+			}
 		}
+	default:
+		// STATUS CODE: 400
+		responseWriter.WriteHeader(http.StatusBadRequest)
 	}
 	
 }
